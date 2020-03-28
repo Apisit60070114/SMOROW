9690d783e86b400630895e1dc2d80e64894290ef
FOR.FOSE <32724812+Apisit60070114@users.noreply.github.com>
2020-03-28 23:16:29 +0700

    1

---
+++ script.js
@@ -0,0 +1,4 @@
+function mockupClick(pageToHide, pageToShow) {
+  document.querySelector('#' + pageToHide).style.display = "none"; 
+  document.querySelector('#' + pageToShow).style.display = "block";
+}
